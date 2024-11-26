77a8292deb94970ee5685b2a0c46e8f51ab821c7
Yasir Arafat <https://github.com/Yasir390/To-Do-APP.git>
2024-11-27 00:07:33 +0600

    math calculations on going

---
+++ 1basics/increment_decrement.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int x,y;
+    float z;
+
+     cout<< x+y;
+
+     // post increment all are same thing
+     x++;
+     x = x+1;
+     x +=1;
+    
+    // pre increment
+     ++x;
+
+     // post decrement all are same thing
+     x--;
+     x = x-1;
+     x -=1;
+
+
+     x++ ;// it means at first use value of x , then increment
+     ++x ;// it means at first increment the value of x , the use 
+}
