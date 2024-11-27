499e6373aa012d86098058eab4eb8e9da85b8286
Yasir Arafat <https://github.com/Yasir390/To-Do-APP.git>
2024-11-27 23:51:25 +0600

    character type start

---
+++ 1basics/typecasting.cpp
@@ -0,0 +1,15 @@
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int x;
+   cout<<"enter a number : ";
+   cin >> x;
+    //type casting int to float
+    float y = (float)x; 
+   cout<<y/2;
+ 
+
+    return 0;
+}
---
+++ 1basics/userInput.cpp
@@ -0,0 +1,18 @@
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int x,y;
+   cout<<"enter 1st number : ";
+   cin >> x;
+   
+    
+   cout<<"enter 2nd number : ";
+   cin >>y;
+   
+   int sum = x+y;
+   cout << "summation is : " << sum;
+
+    return 0;
+}
