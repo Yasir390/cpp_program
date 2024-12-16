9436d79d7e82bff80d4d5c40ee6c51da2bc325d7
Yasir Arafat <https://github.com/Yasir390/To-Do-APP.git>
2024-12-17 00:22:29 +0600

    recursion on going

---
+++ ch7_recursion/1_to_n_print_recursion.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+ void  printFun(int x,int n){
+    if(x> n) return;
+        cout<<x<<endl;
+       printFun(x+1,n);
+    }
+
+int main(){
+    int n;
+    cout<<"Enter n: ";
+    cin>> n; 
+
+    printFun(1,n);
+}
