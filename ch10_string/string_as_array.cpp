a47dc705532c3f02fe454a14879d196e7be1e607
Yasir Arafat <https://github.com/Yasir390/To-Do-APP.git>
2024-12-24 00:37:16 +0600

    string on going and stl

---
+++ ch10_string/string_as_array.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+
+    string str = "My name is Yasir Arafat";
+    string s = "Arafa";
+    cout<<str[1]<<endl;
+    cout<<s.length();
+
+    //update
+    str[3] = 'k';
+    cout<<str;
+
+
+}
